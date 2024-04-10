f986260a055cb50c7a6e319e9b0729490e96b0ce
wkornatowski <wkornatowski@users.noreply.github.com>
2024-04-10 15:45:48 +0100

    Update postgresQueries.sql

--- postgresQueries.sql
+++ postgresQueries.sql
@@ -5,11 +5,20 @@ AND state = 'active'
 ORDER BY duration DESC;
 
 
--- Enable the extension if it's not already enabled
-CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-
--- Query to find high CPU usage queries based on total time
-SELECT query, calls, total_time, rows, 100.0 * total_time / sum(total_time) OVER () AS perc_total_time
-FROM pg_stat_statements
-ORDER BY total_time DESC
-LIMIT 10;
+SELECT 
+    a.pid, 
+    now() - a.query_start AS duration, 
+    a.query, 
+    a.state,
+    s.total_time, 
+    s.calls, 
+    s.rows 
+FROM 
+    pg_stat_activity a
+JOIN 
+    pg_stat_statements s ON a.query = s.query 
+WHERE 
+    (now() - a.query_start) > interval '5 minutes' 
+    AND a.state = 'active'
+ORDER BY 
+    duration DESC;
