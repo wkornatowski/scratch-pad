a3726c1ee06255380aafba38abc915bb44cb574c
wkornatowski <wkornatowski@users.noreply.github.com>
2024-04-10 15:40:47 +0100

    Create postgresQueries.sql

---
+++ postgresQueries.sql
@@ -0,0 +1,5 @@
+SELECT pid, now() - pg_stat_activity.query_start AS duration, query, state
+FROM pg_stat_activity
+WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes'
+AND state = 'active'
+ORDER BY duration DESC;
